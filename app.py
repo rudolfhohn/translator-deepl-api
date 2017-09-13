764cc4aa3d925b32451ac27b41fa8a79b8f04c87
Rudolf Höhn <rudolf.hohn@gmail.com>
2017-09-13 09:06:09 +0200

    feat(translation): translate text to french
    Add support for DeepL translation from autodection to french.

--- app.py
+++ app.py
@@ -2,16 +2,110 @@ from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+import pydeepl
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
 
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "translateText":
+        return {}
+
+    text = req.get('result').get('parameters').get('text')
+    translation = pydeepl.translate(text, 'FR')
+    return {
+        'speech': translation,
+        'displayText': translation,
+        'source': 'translator-deepl-api'
+    }
+    # baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    # yql_query = makeYqlQuery(req)
+    # if yql_query is None:
+    #     return {}
+    # yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    # result = urlopen(yql_url).read()
+    # data = json.loads(result)
+    # res = makeWebhookResult(data)
+    # return res
+
+
+# def makeYqlQuery(req):
+#     result = req.get("result")
+#     parameters = result.get("parameters")
+#     city = parameters.get("geo-city")
+#     if city is None:
+#         return None
+#
+#     return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+#
+#
+# def makeWebhookResult(data):
+#     query = data.get('query')
+#     if query is None:
+#         return {}
+#
+#     result = query.get('results')
+#     if result is None:
+#         return {}
+#
+#     channel = result.get('channel')
+#     if channel is None:
+#         return {}
+#
+#     item = channel.get('item')
+#     location = channel.get('location')
+#     units = channel.get('units')
+#     if (location is None) or (item is None) or (units is None):
+#         return {}
+#
+#     condition = item.get('condition')
+#     if condition is None:
+#         return {}
+#
+#     # print(json.dumps(item, indent=4))
+#
+#     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+#              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+#
+#     print("Response:")
+#     print(speech)
+#
+#     return {
+#         "speech": speech,
+#         "displayText": speech,
+#         # "data": data,
+#         # "contextOut": [],
+#         "source": "apiai-weather-webhook-sample"
+#     }
+
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
